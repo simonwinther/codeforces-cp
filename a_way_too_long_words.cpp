3ca3c5a6d1e6e8c768473056df8e0c974d44b050
Simon Winther <simonwa01@gmail.com>
2025-11-11 11:12:55 +0100

    finished problemset

---
+++ a_way_too_long_words.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  while (x--) {
+    string s;
+    cin >> s;
+
+    if (s.size() <= 10) {
+      cout << s << endl;
+    } else {
+      cout << s[0] << s.size() - 2 << s[s.size() - 1] << endl;
+    }
+  }
+}
