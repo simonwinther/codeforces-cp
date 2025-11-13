023bc60f3fcbd816eee89bb78182675b121f9439
simon <simonwa01@gmail.com>
2025-11-13 20:12:12 +0100

    solved quick one for today (setup gh too so spend more than 1 hour)

--- a_boy_or_girl.cpp
+++ a_boy_or_girl.cpp
@@ -5,10 +5,15 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  set<int> s1;
+  set<char> s1;
   string s;
   cin >> s;
+
   for (auto c : s) {
+    s1.insert(c);
   }
+
+  cout << (s1.size() % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!") << endl;
+
   return 0;
 }
