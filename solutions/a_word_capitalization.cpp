7fde37e0bed9b6ebc772e4d95b7cf338bfb40c60
simon <simonwa01@gmail.com>
2025-11-14 07:57:35 +0100

    a_word_capitalization solved

---
+++ solutions/a_word_capitalization.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  s[0] = toupper(s[0]);
+  cout << s << endl;
+  return 0;
+}
